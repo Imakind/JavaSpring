5c3319eafa2c60cfb12fa980a98e5024f85d1a7c
Akishev Akzhan <akishevakzhan@gmail.com>
2025-03-21 15:47:45 +0500

    first commit

---
+++ src/main/java/ru/alishev/springcourse/ClassicalMusic.java
@@ -0,0 +1,15 @@
+package ru.alishev.springcourse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Neil Alishev
+ */
+@Component
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ src/main/java/ru/alishev/springcourse/MusicPlayer.java
@@ -0,0 +1,41 @@
+package ru.alishev.springcourse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Neil Alishev
+ */
+@Component
+public class MusicPlayer {
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    @Autowired
+    @Qualifier("rockMusic")
+    private Music music;
+
+    public void playMusic() {
+        System.out.println("Playing: " + music.getSong());
+    }
+}
